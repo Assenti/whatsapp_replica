bb7bf6cf4038d6a0c4f76bda3a763d9ae03dd31f
Asset Sultanov <asset.sultan@gmail.com>
2019-06-20 20:25:42 +0600

    Chats list item design implemented

--- src/components/ChatsList.vue
+++ src/components/ChatsList.vue
@@ -1,16 +1,30 @@
 <template>
     <div class="chats-list">
-        <div v-for="(chat, index) in 30" :key="index" class="chats-list-item">
+        <div v-for="(chat, index) in 30" 
+            :key="index" 
+            class="chats-list-item">
             <div class="chats-list-item-avatar">
-                <!-- <img src="../assets/user.svg" alt="avatar"> -->
+                <img :title="iconAuthor" src="../assets/user.png" 
+                    alt="avatar">
             </div>
-            <div class="chats-list-item-content"></div>
+            <div class="chats-list-item-content">
+                <div class="chats-list-item-title">
+                    <div>Tony Stark</div>
+                    <span>01/01/2019</span>
+                </div>
+                <div class="chats-list-item-subtitle">Last message of chat</div>
+            </div>
         </div>
     </div>
 </template>
 
+
 <script>
 export default {
-    
+    data() {
+        return {
+            iconAuthor: 'Icons from https://www.flaticon.com/authors/smashicons'
+        }
+    }
 }
 </script>
